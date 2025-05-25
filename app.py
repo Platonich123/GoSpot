32e347d522e146d9d89af5ad5fc6e1fd0a0aa98f
Platonich123 <145435938+Platonich123@users.noreply.github.com>
2025-05-25 13:48:44 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 # Конфигурация приложения
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', os.urandom(24))
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}?client_encoding=utf8"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT', '5432')}/{os.getenv('DB_NAME')}?client_encoding=utf8"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Настройка для продакшена
